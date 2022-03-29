f9eebf585b584cfb03c4e7ce9df717fcf0efbd9b
mehedi-hasan-24 <hmehedi203@gmail.com>
2022-03-29 16:32:40 +0600

    added react router v6 and some dummy code

---
+++ src/components/Parent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Parent(props) {
+    console.log(props);
+    return (
+        <div>
+            Parent
+        </div>
+    );
+}
+
+export default Parent;

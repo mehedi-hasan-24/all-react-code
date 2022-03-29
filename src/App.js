4a3dcc9eef769c02538080bc687c9f816e8e9726
mehedi-hasan-24 <hmehedi203@gmail.com>
2022-03-29 16:32:29 +0600

    added react router v6 and some dummy code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Parent from "./components/Parent";
+import Child from "./components/Child";
+import {Component} from "react";
 
 function App() {
   return (
     <div >
-      HI
+        <ClassComponent></ClassComponent>
+        <ClassComponent></ClassComponent>
     </div>
   );
+}
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color} style={{backgroundColor: props.color}}>
+            {props.children}
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="green">
+            <h1 className="Dialog-title">
+                Welcome
+            </h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+            </p>
+        </FancyBorder>
+    );
+}
+
+class ClassComponent extends Component {
+    constructor(props) {
+        super(props);
+        console.log("constructor called!")
+    }
+    render() {
+        return (
+            <div>
+                Class component
+            </div>
+        );
+    }
 }
 
 export default App;

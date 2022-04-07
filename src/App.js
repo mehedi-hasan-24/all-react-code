7ae7542160a2e1f4837b1c867b48001c64e8e478
mehedi-hasan-24 <hmehedi203@gmail.com>
2022-04-07 17:43:30 +0600

    basic hoc pattern completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ClickCounter from "./component/ClickCounter";
 
 function App() {
   return (
     <div >
-      HI
+      <ClickCounter />
     </div>
   );
 }

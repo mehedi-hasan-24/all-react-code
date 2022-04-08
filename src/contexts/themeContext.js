8190512ce1d5542899c26c28483b450df1b4418b
mehedi-hasan-24 <hmehedi203@gmail.com>
2022-04-08 13:55:45 +0600

    replaced custom context api with react context api

--- src/contexts/themeContext.js
+++ src/contexts/themeContext.js
@@ -1,6 +1,7 @@
-import createContext from "../lib/Context";
+// import createContext from "../lib/Context";
+import React from "react";
 
-const themeContext = createContext({
+const themeContext = React.createContext({
     theme: 'light',
 });
 export default themeContext;
